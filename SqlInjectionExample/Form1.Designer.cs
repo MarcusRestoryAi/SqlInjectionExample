5f9fbdb59dca0136400966e4cb8e3d56c8006f08
Marcus <marcus.henriksson@restory.ai>
2024-05-30 14:39:26 +0200

    Add project files.

---
+++ SqlInjectionExample/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SqlInjectionExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblCrypt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(217, 32);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(364, 45);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(217, 120);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(364, 45);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(217, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Logga in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(28, 293);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(298, 38);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "Du är inte inloggad!";
+            // 
+            // lblCrypt
+            // 
+            this.lblCrypt.AutoSize = true;
+            this.lblCrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCrypt.Location = new System.Drawing.Point(35, 353);
+            this.lblCrypt.Name = "lblCrypt";
+            this.lblCrypt.Size = new System.Drawing.Size(0, 39);
+            this.lblCrypt.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 532);
+            this.Controls.Add(this.lblCrypt);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblCrypt;
+    }
+}
+
---
+++ SqlInjectionExample/Form1.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlInjectionExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoggaIn();
+        }
+
+        private void LoggaIn()
+        {
+
+            //Definiera min Connection String
+            string server = "localhost";
+            string database = "sqlinkectexample";
+
+            string dbUsername = "injectuser";
+            string dbPassword = "ABC123";
+
+            string MySqlConnectionString = $"SERVER={server};DATABASE={database};UID={dbUsername};PASSWORD={dbPassword};";
+
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            //string sqlQuerry = $"SELECT * from users WHERE users_username = '{username}' and users_password = '{password}';";
+            string sqlQuerry = "SELECT * from users WHERE users_username = @username and users_password = @password;";
+
+            //Skapa MySQL connection
+            MySqlConnection conn = new MySqlConnection(MySqlConnectionString);
+
+            //Skapa en MySQL command
+            MySqlCommand cmd = new MySqlCommand(sqlQuerry, conn);
+
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            try
+            {
+                //Öppna connection
+                conn.Open();
+
+                //Exekvera command
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read() )
+                {
+                    lblOutput.Text = "Du har loggat in!";
+                } else
+                {
+                    lblOutput.Text = "Du är inte inloggad!";
+                }
+            } catch (Exception e)
+            {
+                lblOutput.Text = e.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            //Kryptera lösenordet, bara för att
+            SHA512 sha = SHA512.Create();
+            Byte[] hash = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+            string hex = BitConverter.ToString(hash).Replace("-", "");
+
+            lblCrypt.Text = hex;
+            
+        }
+    }
+}
